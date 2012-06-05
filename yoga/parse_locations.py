42cafba43c62c7a1df08f9286584766b1b823f7c
Thomas Levine <occurrence@thomaslevine.com>
2012-06-05 12:25:48 -0400

    aoeu

--- yoga/parse_locations.py
+++ yoga/parse_locations.py
@@ -14,10 +14,36 @@ def parse(page_number):
     _check_header(trs.pop(0))
 
     # Body
-    print tostring(trs[0])
-    #.text_content()
+    d = _parse_tr(trs[0])
 
-def _check_header(tr)
+def _parse_tr(tr):
+    "Turn a tr lxml element into a zip"
+    spans = tr.cssselect('span')
+
+    if len(spans) != 13:
+        print(tostring(tr))
+        raise ValueError('Wrong number of spans')
+
+    print [span.attrib['id'] for span in spans]
+    return [_get_span_tuple(span) for span in spans]
+
+KEYS = []
+def _get_span_tuple(span):
+    """
+    Return a tuple of data key and value for a span inside the table.
+    Before returning, check that the key is valid.
+    """
+    key = span.attrib['id'].split('_lbl')[-1]
+    if key not in KEYS:
+        raise ValueError('Key %s is not among the expected keys.' % key)
+
+    # .text_content rather than .text in case one of them is weird.
+    # links, inside of a tags, are an example of such weirdness.
+    value = span.text_content()
+
+    return (key, value)
+
+def _check_header(tr):
     header_tr = tr
     header = [th.text for th in header_tr.xpath('th')]
     if keys != ['Name', 'Contact', 'Address', 'Registration']
