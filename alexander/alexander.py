e8f833e1ec90889ae4f1c8a2e345b190e1073f7d
Thomas Levine <occurrence@thomaslevine.com>
2012-06-11 00:39:47 -0400

    alexander teacher scraper

---
+++ alexander/alexander.py
@@ -0,0 +1,28 @@
+from lxml.html import parse
+from dumptruck import DumpTruck
+
+dt = DumpTruck(dbname = 'alexander.sqlite')
+
+def main():
+    html = parse('http://www.alexandertechnique.com/teacher/northamerica').getroot()
+    states = html.xpath('//tr[td/font/a[@name]]')
+
+    data = []
+    for state in states:
+        name = state.text_content().strip()
+        teachers = state.xpath('following-sibling::tr/descendant::a')
+        for teacher in teachers:
+            city = teacher.text_content().strip()
+            link = teacher.attrib['href']
+            if link[0] != '#':
+                data.append({
+                    'state': name,
+                    'city': city,
+                    'teacher_url': link,
+                })
+    dt.create_table(data[0])
+    dt.create_index('alexander_teachers',
+        ['state', 'city', 'teacher_url'],
+        unique = True, if_not_exists = True,
+    )
+    dt.insert(data, 'alexander_teachers')
