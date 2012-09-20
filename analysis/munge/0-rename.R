1d2e24d886f0940fc896a8f026bb26ea4a693180
Thomas Levine <occurrence@thomaslevine.com>
2012-09-19 22:25:22 -0400

    munging

---
+++ analysis/munge/0-rename.R
@@ -0,0 +1,2 @@
+yoga <- dumptruck
+rm(dumptruck)
---
+++ analysis/munge/3-zip.R
@@ -0,0 +1,7 @@
+get.zip.code <- function(messy.zip) {
+  sub('[^0-9]*([0-9]{5}).*', '\\1', messey.zip)
+}
+
+# Get the zip code
+aff$zip <- get.zip.code(aff$GCT_STUB.display.label.1)
+yoga$zip <- get.zip.code(yoga$Zip)
