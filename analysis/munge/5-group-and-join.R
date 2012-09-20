f290f35ff1aee102a4e035a3a7a9edf428423db9
Thomas Levine <occurrence@thomaslevine.com>
2012-09-19 22:47:20 -0400

    median

--- analysis/munge/5-group-and-join.R
+++ analysis/munge/5-group-and-join.R
@@ -1,7 +1,8 @@
+library(isotone)
 aff.grouped <- ddply(aff, 'zip', function(df){
   c(
     pop = sum(df$Total.population),
-    pseudo.median.age = median(df$Median.age..years),
+    median.age = weighted.median(df$Median.age..years, df$Total.population),
     portion.female = weighted.mean(df$portion.female, df$Total.population)
   )
 })
