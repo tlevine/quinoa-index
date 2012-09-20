9d0f00056c624bb70f9b29abab35669e6e1a9a50
Thomas Levine <occurrence@thomaslevine.com>
2012-09-19 23:45:11 -0400

    plot gender balance

--- analysis/src/eda.R
+++ analysis/src/eda.R
@@ -1,5 +1,5 @@
 library('ProjectTemplate')
-load.project()
+# load.project()
 
 for (dataset in project.info$data)
 {
@@ -12,3 +12,18 @@ print(yoga.top.1[1:5,])
 
 yoga.top.2 <- subset(quinoa[order(quinoa$yoga.teachers.per.capita, decreasing = T),],yoga.teachers > 1)[1:10,]
 print(yoga.top.2[1:5,])
+
+gender.imbalance <- quinoa[order(abs(quinoa$portion.female - 0.5), decreasing = T),][1:1000,]
+gender.imbalance.plot <- ggplot(quinoa) +
+  aes(y=(pop*(1-portion.female)), x=(pop*portion.female)) +
+  geom_abline(slope=1, intercept = 0) +
+  # Dunno what's wrong with this
+# opts(
+#   plot.title = 'Zip code gender balance'
+#   plot.title = 'Out-lying gender imbalances tend towards males with high populations and vice-versa',
+#   axis.title.x = 'Females within a particular zip code',
+#   axis.title.y = 'Males within a particular zip code'
+# ) +
+  geom_point()
+
+print(gender.imbalance.plot)
