b52a98fee37f187e1719530063ce75cd6ddbdc8c
Thomas Levine <occurrence@thomaslevine.com>
2012-09-19 23:17:36 -0400

    move stuff to src

--- analysis/munge/9-present.R
+++
@@ -1,1 +0,0 @@
-yoga.top.100 <- quinoa[order(quinoa$yoga.teachers.per.capita, decreasing = T),][1:100,]
--- analysis/src/eda.R
+++ analysis/src/eda.R
@@ -6,3 +6,6 @@ for (dataset in project.info$data)
   message(paste('Showing top 5 rows of', dataset))
   print(head(get(dataset)))
 }
+
+yoga.top.100 <- quinoa[order(quinoa$yoga.teachers.per.capita, decreasing = T),][1:100,]
+print(yoga.top.100[1:5,])
